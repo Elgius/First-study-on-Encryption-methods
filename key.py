312d0aea449adbf64e232593685d3826b888cbf9
Elgius <114131653+Elgius@users.noreply.github.com>
2022-11-16 12:00:32 +0500

    Updated functionality

--- key.py
+++ key.py
@@ -1,15 +1,52 @@
 
+def Monitor(Encrypted_messag):
+    cryption = Encrypted_messag
 
-#def Encryptor():
+    #keeps track of easily missable characters like space and commas
+    ticket = 0
 
-txt = "Hello World yeet"
+    if " " in cryption:
+        print("TRUE")
+        ticket = ticket +1
 
-txt_list = []
+    if "," in cryption:
+        print("TRUE")
+        ticket = ticket +1
 
-fresh_txt = txt.replace(" ","")
+    if "." in cryption:
+        print("TRUE")
+        ticket = ticket +1
 
-for i in fresh_txt:
-    txt_list.append(i)
 
-print (txt_list)
-    
+
+    print(f'this is the code: {cryption }, Do note this is the amount of tickets noticed = {ticket}')
+
+
+
+def Encrypt(txt):
+    shift_value = 5
+    Encrypted_message = ""
+
+    message = txt
+
+    for i in message:
+        code = ord(i)
+
+        if (code == 32):
+            print("Spacing detected")
+
+        if (code == 44):
+            print("comma detected")
+
+        new_code = code + shift_value
+        new_letter = chr(new_code)
+        Encrypted_message = Encrypted_message + new_letter
+
+    Monitor(Encrypted_message)
+
+    return Encrypted_message
+
+
+
+txt = input("Enter the word here:  ")
+Encrypt(txt)
